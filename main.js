3988661c01140b466d9ced5090fc5808a0a76176
prince-i <prince_arce01@outlook.com>
2021-10-12 14:30:08 +0800

    PROFILE

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
              typeWriter();
             document.querySelector('#loader').classList.remove("loader");
             $('.tooltipped').tooltip();
+            get_theme();
 });
         $(window).on("scroll", function(){
             amountscrolled();
@@ -22,9 +23,7 @@ $(document).ready(function(){
         var trackLength = docheight - winheight;
         var pctScrolled = Math.floor(scrollTop/trackLength * 100);
         // console.log(pctScrolled + '% scrolled');
-
     }
-    
         // TYPEWRITER EFFECT
         var i = 0;
         var txt = 'PHP Developer - IT Technical Support - UX/UI Designer';
@@ -36,4 +35,59 @@ $(document).ready(function(){
             setTimeout(typeWriter, speed);
             }
         }
-        
+        const get_theme =()=>{
+          localStorage.getItem("theme");
+          var theme = localStorage.theme;
+          console.log(theme);
+          if(theme == 'dark'){
+            $('#theme_button').html('Light Mode<i class="material-icons white-text">brightness_5<i>');
+            $('#theme_button_nav').html('<i class="material-icons white-text">brightness_5<i>');
+            $('nav').removeClass('white');
+            $('nav').addClass('#212121 grey darken-4');
+            $('nav a').addClass('white-text');
+            $('.brand-logo').addClass('white-text');
+            $('.sidenav-trigger').addClass('white-text');
+            $('.sidenav').addClass('#212121 grey darken-4');
+            $('.sidenav a').addClass('white-text');
+            $('body').addClass('#212121 grey darken-4');
+            $('body').addClass('white-text');
+            $('#work .card').addClass('N/A transparent');
+            $('#title').removeClass('grey-text text-darken-3 lighten-3');
+            $('#title').addClass('white-text');
+          }else{
+            $('#theme_button').html('Dark Mode<i class="material-icons black-text">brightness_2<i>');
+            $('#theme_button_nav').html('<i class="material-icons black-text">brightness_2<i>');
+            $('nav').addClass('white');
+            $('nav').removeClass('#212121 grey darken-4');
+            $('nav a').removeClass('white-text');
+              $('nav a').addClass('black-text');
+            $('.brand-logo').removeClass('white-text');
+            $('.sidenav-trigger').removeClass('white-text');
+            $('.sidenav').removeClass('#212121 grey darken-4');
+            $('.sidenav a').removeClass('white-text');
+            $('body').removeClass('#212121 grey darken-4');
+            $('body').removeClass('white-text');
+            $('#work .card').removeClass('N/A transparent');
+            $('#title').addClass('grey-text text-darken-3 lighten-3');
+            $('#title').removeClass('white-text');
+          }
+        }
+
+        // TOGGLE LIGHT MODE AND DARK MODE
+        $('#theme_button').click(function(){
+          if(localStorage.getItem("theme") == '' || localStorage.getItem("theme") == undefined || localStorage.getItem("theme") == 'light'){
+            localStorage.setItem("theme","dark");
+          }else{
+            localStorage.setItem("theme","light");
+          }
+          get_theme();
+        });
+
+        $('#theme_button_nav').click(function(){
+          if(localStorage.getItem("theme") == '' || localStorage.getItem("theme") == undefined || localStorage.getItem("theme") == 'light'){
+            localStorage.setItem("theme","dark");
+          }else{
+            localStorage.setItem("theme","light");
+          }
+          get_theme();
+        });
