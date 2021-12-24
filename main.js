7ff49603ba44268b87c8ba6cdb2f5326ae2aad4f
prince-i <prince_arce01@outlook.com>
2021-12-24 16:39:11 +0800

    UPDATE

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'IT PROGRAMMER/ SOFTWARE DEVELOPER/ IT TECHNICIAN/ UX DESIGNER/ ENTREPRENEUR';
+        var txt = 'SOFTWARE DEVELOPER/ IT TECHNICIAN/ UX DESIGNER/ ENTREPRENEUR';
         var speed = 50;
         function typeWriter() {
         if (i < txt.length) {
