edee8ea4cc441be7e1b03903a8a45389643829b9
prince-i <prince_arce01@outlook.com>
2022-03-01 12:29:45 +0800

    UPDATE

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'IT PROGRAMMER/ IT TECH SUPPORT/ DATA ANALYST/ UI DESIGNER/ MOBILE PHOTOGRAPHER';
+        var txt = 'IT PROGRAMMER/ IT TECH SUPPORT/ DATA ANALYST/ DIGITAL DESIGNER/ MOBILE PHOTOGRAPHER';
         var speed = 50;
         function typeWriter() {
         if (i < txt.length) {
