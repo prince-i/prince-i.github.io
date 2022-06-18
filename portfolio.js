453a7689165138f4a15473b2919c741c7e8d3e5f
prince-i <prince_arce01@outlook.com>
2022-06-18 15:23:10 +0800

    UPDATE

--- portfolio.js
+++ portfolio.js
@@ -21,8 +21,9 @@ let portfolio = [
 	['Image/systems/pcs_summary_view.png','PROGRESS COUNTER SUMMARY','Web Application / August 2021'],
 	['Image/systems/hr-aris.png','HR Absenteeism Report Information System','Web Application / September 2021'],
 	['Image/systems/pcamera.png','pCAMERA - Image Classification Camera','Android App / November 2021'],
-	['Image/systems/faculty_scheduling_system.png','School Faculty Scheduling System','Web App (Debug) / December 2021'],
-	['Image/systems/homeseek_admin.png','HomeSeek Admin','Web App / February 2021']
+	['Image/systems/faculty_scheduling_system.png','School Faculty Scheduling System','Web App(Debug) / December 2021'],
+	['Image/systems/homeseek_admin.png','HomeSeek Admin','Web App / February 2022'],
+	['Image/systems/library_management_sys_qr.jpeg','Library Management System w/ QR Code','Web App (Project Manager)/ June 2022']
 ];
 
 
