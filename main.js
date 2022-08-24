0044e6f6c6951e046b0a3535e290cb818966625c
prince-i <prince_arce01@outlook.com>
2022-08-24 21:04:31 +0800

    UPDATE

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'SYSTEMS DEVELOPER | SQL DEVELOPER | IT SPECIALIST | GRAPHIC DESIGNER';
+        var txt = 'IT PROGRAMMER | GRAPHIC DESIGNER';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
