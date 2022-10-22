70d33ad464f5c47ba396534b8d2fcf9e5fa19c43
prince-i <prince_arce01@outlook.com>
2022-10-22 11:40:12 +0800

    UPDATE

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PROGRAMMER | IT SUPPORT | SQL DEVELOPER | GRAPHIC DESIGNER';
+        var txt = 'PROGRAMMER & TECH GUY';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
