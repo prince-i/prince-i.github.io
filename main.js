6206b6583c28571454434396ddeeab5105b1bf56
prince-i <prince_arce01@outlook.com>
2022-11-12 13:59:13 +0800

    UPDATE

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'IT Programmer | Configuration Technician |DataOps | Graphic Designer';
+        var txt = 'IT Programmer | Configuration Technician | DataOps | Graphic Designer';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
