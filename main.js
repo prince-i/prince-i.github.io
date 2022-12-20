9e95365c57a67f3958a05d89f514b037d22289fa
prince-i <prince_arce01@outlook.com>
2022-12-20 15:09:17 +0800

    UPDATE

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PROGRAMMER | IT ANALYST | VISUAL DESIGNER';
+        var txt = 'PROGRAMMER | TECH ANALYST | VISUAL DESIGNER';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
