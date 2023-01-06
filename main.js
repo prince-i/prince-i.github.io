e8450ae3ab4dd7728e0bff7329dab1c513915b64
prince-i <prince_arce01@outlook.com>
2023-01-06 17:35:40 +0800

    UPDATE

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'COMPUTER PROGRAMMER | IT CONSULTANT | VISUAL DESIGNER';
+        var txt = 'COMPUTER PROGRAMMER | TECH CONSULTANT | VISUAL DESIGNER';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
