bf827802d959e49f165c9b4553467d48517150fb
prince-i <prince_arce01@outlook.com>
2023-07-13 22:33:20 +0800

    UPDATE

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PROGRAMMER | IT CONSULTANT | VISUAL DESIGNER';
+        var txt = 'PROGRAMMER | IT CONSULTANT | UX DESIGNER';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
