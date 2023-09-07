b98444a87d4b24bc3a9946ef26cac86388077c72
prince-i <prince_arce01@outlook.com>
2023-09-07 10:20:27 +0800

    UPDATE

--- portfolio.js
+++ portfolio.js
@@ -78,6 +78,7 @@ let upskill = [
 	['DataOps Fundamentals Certification','Nov 21, 2022','Cert No: 18422660104','https://learn.datakitchen.io/certificate/ODIwMDBfMTExNDMxMg'],
 	['Introduction To Information Technology For Managers','Aug 24, 2023','Cert No: 1757-17286154','https://alison.com/certification/check/$2y$10$TQOfQLtyijSdyYP4TK4ciuAv8mX0NDHnPGAaEp1A0Gb39viNCtuW'],
 	['ISO/IEC 27001 - Dynamics of Information Security Management System (ISMS)','Aug 30, 2023','Cert No: 2251-17286154','https://alison.com/certification/check/$2y$10$FpSsfjnqgVdt1FLoKdrkrO6rPKiQref04bH8gB6rSkN5G0D3DncIq'],
+	['Diploma in Information Technology Technician','Sept 07, 2023','Cert No: 5993-17286154','https://alison.com/certification/check/2y100qFj14JsChp9NvrPSPW19ejV0ZOPdMSq9SsgQi8aE3TjiK1JQeXAm'],
 
 
 ];
