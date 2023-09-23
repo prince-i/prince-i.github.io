860a4fffd9d4727233e0c56f6728ad263b70d160
prince-i <prince_arce01@outlook.com>
2023-09-23 17:52:11 +0800

    UPDATE

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PROGRAMMER | SYSADMIN | VISUAL DESIGNER';
+        var txt = 'PROGRAMMER | SYSADMIN | VISUAL DESIGNER | INFOSEC ENTHUSIAST';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
