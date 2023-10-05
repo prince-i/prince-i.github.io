0f0d25ac6ee3c4e3959d90e77641991c7a753a23
prince-i <prince_arce01@outlook.com>
2023-10-05 15:33:47 +0800

    UPDATE

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PROGRAMMER | SYSADMIN | INFOSEC ENTHUSIAST | VISUAL DESIGNER';
+        var txt = 'PROGRAMMER | SYSADMIN | SOC PRACTITIONER | DIGITAL DESIGNER';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
