f01750585319c6cbe2287fca6786ab03df8dcde9
prince-i <prince_arce01@outlook.com>
2023-12-31 17:36:20 +0800

    UPDATE

--- portfolio.js
+++ portfolio.js
@@ -53,6 +53,7 @@ let tech_stack = [
 	['Image/icons/bash.png','Bash'],
 	['Image/icons/office.png','Microsoft 365'],
 	['Image/icons/Msdos-icon.svg','MS-DOS'],
+	['Image/icons/appscript.jpeg','App Script'],
 
 ];
 
