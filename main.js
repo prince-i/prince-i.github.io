e44485edb69df903bce53cb1f3433733a880a101
prince-i <prince_arce01@outlook.com>
2024-01-29 16:16:24 +0800

    UPDATE

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PHP PROGRAMMER | PROJECT MANAGER | TECH CONTRACTOR | DIGITAL DESIGNER';
+        var txt = 'PHP DEVELOPER | PROJECT MANAGER | TECH CONTRACTOR | DIGITAL DESIGNER';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
