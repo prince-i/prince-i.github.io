19b6c89d6af32c065e0e225d06c8e676584f7620
prince-i <prince_arce01@outlook.com>
2024-05-14 21:03:25 +0800

    UPDATE

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PROGRAMMER | IT CONSULTANT';
+        var txt = 'PROGRAMMER | IT CONSULTANT | TECH';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
