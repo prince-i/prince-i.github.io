37bfa81fce6771f17bc2af1bb386ffe8d84da70d
prince-i <prince_arce01@outlook.com>
2024-08-01 23:20:34 +0800

    UPDATE

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ $(document).ready(function(){
 
         // TYPEWRITER EFFECT
         var i = 0;
-        var txt = 'PROGRAMMER | TECH LEAD | SYSTEM TECHNICIAN';
+        var txt = 'PROGRAMMER | TECH LEAD | IT TECHNICIAN';
         var speed = 20;
         function typeWriter() {
         if (i < txt.length) {
