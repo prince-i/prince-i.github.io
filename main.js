202108f756b6f5c12dd374c097cce2a528fbb777
prince-i <prince_arce01@outlook.com>
2024-12-13 18:33:54 +0800

    UPDATE

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ for (var n = 0; n <= networks.length - 1; n++) {
 
 // TYPEWRITER EFFECT
 let i = 0;
-const txt = 'JUST A SIMPLE IT GUY ... 👾';
+const txt = ' A COMPUTER PROGRAMMER 👾, WHO LOVE CATS ฅ≽^•⩊•^≼ฅ';
 const speed = 100;
 
 function typeWriter() {
