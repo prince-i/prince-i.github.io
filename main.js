1da30e203a9cb7de997714279e1ad5932c8a846e
prince-i <prince_arce01@outlook.com>
2024-12-21 19:13:33 +0800

    UPDATE

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ for (var n = 0; n <= networks.length - 1; n++) {
 
 // TYPEWRITER EFFECT
 let i = 0;
-const txt = 'COMPUTER PROGRAMMER | TECHNICAL LEAD | TECHNICIAN';
+const txt = 'COMPUTER PROGRAMMER | TECHNICAL LEAD | TECHNICIAN | DIGITAL DESIGNER';
 const speed = 100;
 
 function typeWriter() {
