7f126553b0a7deede2b13dbdae2010eb84d2c225
prince-i <prince_arce01@outlook.com>
2025-05-10 18:32:34 +0800

    UPDATE

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ for (var n = 0; n <= networks.length - 1; n++) {
 
 // TYPEWRITER EFFECT
 let i = 0;
-const txt = 'PROGRAMMER | TECH LEAD';
+const txt = 'PROGRAMMER | TECH LEAD | MIS OFFICER';
 const speed = 15;
 
 function typeWriter() {
--- portfolio.js
+++ portfolio.js
@@ -3,7 +3,7 @@ var about = [
 	['NAME:','PRINCE'],
 	['PROFILE:','PROGRAMMER'],
 	['EDUCATION:','<ul><li>Bachelor of Science in Information Technology: GRADUATED</li><li>Master in Information Technology: ONGOING</li></ul>'],
-	['INDUSTRY KNOWLEDGE:','<ul><li>Data Management & AI</li><li>IT Operations</li><li>Programming</li><li>Project and IT Management</li><li>Tech Support</li><li>Technology and Infrastructure</li><li>Technical Research and Documentation</li></ul>']
+	['INDUSTRY KNOWLEDGE:','<ul><li>Machine Learning</li><li>IT Operations</li><li>Programming</li><li>Project and IT Management</li><li>Tech Support</li><li>Technology and Infrastructure</li><li>Technical Research and Documentation</li></ul>']
 
 ];
 
