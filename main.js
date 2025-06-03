7a37dabce0f732040f3e3e2a39d0389c4a6701b8
prince-i <prince_arce01@outlook.com>
2025-06-03 21:08:31 +0800

    UPDATE

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ for (var n = 0; n <= networks.length - 1; n++) {
 
 // TYPEWRITER EFFECT
 let i = 0;
-const txt = 'PROGRAMMER | TECH CONTRACTOR | WEBOPS';
+const txt = 'Programmer | Tech Contractor | WebOps';
 const speed = 15;
 
 function typeWriter() {
--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
 //DATA IN MULTIDIMENSIONAL ARRAY
 var about = [
-	['NAME:','PRINCE'],
-	['PROFILE:','PROGRAMMER &bull; TECH CONTRACTOR'],
+	['NAME:','Prince'],
+	['PROFILE:','Programmer &bull; Tech Contractor'],
 	['EDUCATION:','<ul><li>Bachelor of Science in Information Technology: GRADUATED</li><li>Master in Information Technology: ONGOING</li></ul>'],
 	['INDUSTRY KNOWLEDGE:','<ul><li>Machine Learning</li><li>IT Operations</li><li>Programming</li><li>Project and IT Management</li><li>Tech Support</li><li>Technology and Infrastructure</li><li>Technical Research and Documentation</li></ul>']
 
