2fbb164656c88cf69ba02a9f052f8a3e2e3f4607
prince-i <prince_arce01@outlook.com>
2025-06-03 21:29:53 +0800

    UPDATE

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ for (var n = 0; n <= networks.length - 1; n++) {
 
 // TYPEWRITER EFFECT
 let i = 0;
-const txt = 'Programmer | Tech Contractor | WebOps';
+const txt = 'PROGRAMMER | TECH CONTRACTOR | WEB ADMIN';
 const speed = 15;
 
 function typeWriter() {
