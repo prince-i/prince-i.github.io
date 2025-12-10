237d022f10b9f18ff7de64e108cf0a3dd75970f9
prince-i <prince_arce01@outlook.com>
2025-12-10 20:25:05 +0800

    UPDATE

--- main.js
+++ main.js
@@ -163,7 +163,7 @@ networkContainer.append(networkHTML);
 // END DATA ------------------------------------------------------------------------------------------------------------------------------------------------
 
 // TYPEWRITER
-const txt = 'PHP DEVELOPER | ETL DEVELOPER';
+const txt = 'PROGRAMMER | ETL DEVELOPER';
 const speed = 30;
 const sChars = "!@#$%^&*()_+-=[]{}|;:',.<>?/`~";
 let i = 0;
